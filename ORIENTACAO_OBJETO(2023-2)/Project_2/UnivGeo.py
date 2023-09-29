d2fb6433107dda9cfd2b53c75aaff3b8a4009512
Natan Almeida <natan.almeida03@gmail.com>
2023-09-29 17:28:02 -0300

    update

--- ORIENTACAO_OBJETO(2023-2)/Project_2/UnivGeo.py
+++ ORIENTACAO_OBJETO(2023-2)/Project_2/UnivGeo.py
@@ -6,7 +6,7 @@ class Point():
     def __init__(self,  x, y):
         self.x = x
         self.y = y
-        self.name = Point.n
+        self._name = Point.n
         Point.n += 1
 
     def update_coord (self, x, y):
@@ -15,42 +15,45 @@ class Point():
         #print("updated ...")
 
     def print_coord (self):
-        print(f'O PONTO {self.name} ESTA LOCALIZADO EM ({self.x}, {self.y})')
+        print(f'O PONTO {self._name} ESTA LOCALIZADO EM ({self.x}, {self.y})')
 
 class Line():
     n = 1
     def __init__(self, ponto1, ponto2):
         self.ponto1 = ponto1
         self.ponto2 = ponto2
-        self.name = Line.n
+        self._name = Line.n
         Line.n += 1
 
     def print_lenght (self):
         c = math.sqrt((self.ponto2.x - self.ponto1.x)**2 + (self.ponto2.y - self.ponto1.y)**2)
-        print(f'COMPRIMENTO DA LINHA {self.name} : {c:.2f}')
+        print(f'COMPRIMENTO DA LINHA {self._name} : {c:.2f}')
 
-class Circle:
+class Circle():
     n = 1
     def __init__(self, centro, raio):
         self.centro = centro
         self.raio = raio
-        self.name = Circle.n
+        self._name = Circle.n
         Circle.n += 1
 
     def update_raio(self, nraio):
         self.raio = nraio
-        #print("updated ...")
+        
+    def update_coord(self, x, y):
+        self.centro.update_coord(x, y)
 
     def print_area(self):
         a = 3.14 * self.raio**2
-        print(f'A ÁREA DO CÍRCULO {self.name} É: {a:.2f}')
+        print(f'A ÁREA DO CÍRCULO {self._name} É: {a:.2f}')
 
     def print_perimeter(self):
         p = 2 * 3.14 * self.raio
-        print(f'O PERÍMETRO DO CÍRCULO {self.name} É: {p:.2f}')
+        print(f'O PERÍMETRO DO CÍRCULO {self._name} É: {p:.2f}')
 
     def print_coord (self):
-        print(f'O CIRCULO {self.name} ESTÁ LOCALIZADO EM: ({self.centro.x}, {self.centro.y})')
+        print(f'O CIRCULO {self._name} TEM SEU CENTRO EM: ({self.centro.x}, {self.centro.y})')
+        print(f'COM O RAIO DE: {self.raio}')
 
 class Triangle():
     n = 1
@@ -58,7 +61,7 @@ class Triangle():
         self.ponto1 = ponto1
         self.ponto2 = ponto2
         self.ponto3 = ponto3
-        self.name = Triangle.n
+        self._name = Triangle.n
         Triangle.n += 1
 
     def print_perimeter(self):
@@ -66,10 +69,10 @@ class Triangle():
         l2 = math.sqrt((self.ponto3.x - self.ponto2.x)**2 + (self.ponto3.y - self.ponto2.y)**2)
         l3 = math.sqrt((self.ponto1.x - self.ponto3.x)**2 + (self.ponto1.y - self.ponto3.y)**2)
         p = l1 + l2 + l3
-        print(f'O PERÍMETRO DO TRIÂNGULO {self.name} É: {p:.2f}')
+        print(f'O PERÍMETRO DO TRIÂNGULO {self._name} É: {p:.2f}')
 
     def print_coord (self):
-        print(f'O TRIANGULO {self.name} ESTÁ LOCALIZADO EM: [({self.ponto1.x}, {self.ponto1.y}), ({self.ponto2.x}, {self.ponto2.y}), ({self.ponto3.x}, {self.ponto3.y})]')
+        print(f'O TRIANGULO {self._name} ESTÁ LOCALIZADO EM: [({self.ponto1.x}, {self.ponto1.y}), ({self.ponto2.x}, {self.ponto2.y}), ({self.ponto3.x}, {self.ponto3.y})]')
 
 def main():
     ponto1 = Point(4, 5)
@@ -104,7 +107,8 @@ def main():
     #circulo2.print_perimeter()
     circulo1.update_raio(2)
     #circulo2.update_raio(4)
-
+    circulo1.update_coord(6, 8)
+    circulo1.print_coord()
     circulo1.print_area()
     #circulo2.print_area()
     circulo1.print_perimeter()
