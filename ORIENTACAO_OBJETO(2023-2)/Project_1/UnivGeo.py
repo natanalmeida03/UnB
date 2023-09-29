1e125d5bd07d3734876311ce6a2e4ed33c5e883b
Natan Almeida <natan.almeida03@gmail.com>
2023-09-29 11:38:13 -0300

    update

--- ORIENTACAO_OBJETO(2023-2)/Project_1/UnivGeo.py
+++ ORIENTACAO_OBJETO(2023-2)/Project_1/UnivGeo.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
+import math
 
 class Ponto():
     def __init__(self,  x, y):
         self.x = x
         self.y = y
+        self.ponto = None
 
     def desenha_ponto (self):
-        plt.scatter(self.x, self.y)
+        self.ponto = plt.scatter(self.x, self.y)
+
+    def set_color (self, cor):
+        self.ponto.set_color(cor)
 
 class Linha():
     def __init__(self, ponto1, ponto2):
@@ -14,7 +19,13 @@ class Linha():
         self.ponto2 = ponto2
 
     def desenha_linha (self):
-        plt.plot([self.ponto1.x, self.ponto2.x], [self.ponto1.y, self.ponto2.y])
+        x = [self.ponto1.x, self.ponto2.x]
+        y = [self.ponto1.y, self.ponto2.y]
+        self.linha = plt.plot(x, y)
+
+    def mostrar_comp (self):
+        c = math.sqrt((self.ponto2.x - self.ponto1.x)**2 + (self.ponto2.y - self.ponto1.y)**2)
+        print("COMPRIMENTO DA LINHA: ", c)
 
 class Triangulo():
     def __init__(self, ponto1, ponto2, ponto3):
@@ -25,7 +36,7 @@ class Triangulo():
     def desenha_triangulo (self):
         x = [self.ponto1.x, self.ponto2.x, self.ponto3.x, self.ponto1.x]
         y = [self.ponto1.y, self.ponto2.y, self.ponto3.y, self.ponto1.y]
-        plt.plot(x, y)
+        self.triangulo = plt.plot(x, y)
 
 class Circulo:
     def __init__(self, centro, raio):
@@ -33,9 +44,8 @@ class Circulo:
         self.raio = raio
 
     def desenha_circulo(self):
-        circulo = plt.Circle((self.centro.x, self.centro.y), self.raio, fill=False)
-        plt.gca().add_patch(circulo)
-
+        self.circulo = plt.Circle((self.centro.x, self.centro.y), self.raio, fill=False)
+        plt.gca().add_patch(self.circulo)
 
 
 ponto1 = Ponto(4, 5)
@@ -44,8 +54,13 @@ triangulo1 = Triangulo(Ponto(2,0), Ponto(1,3), Ponto(4,2))
 circulo1 = Circulo(Ponto(1,5), 1)
 
 ponto1.desenha_ponto()
+ponto1.set_color("pink")
+
 linha1.desenha_linha()
+linha1.mostrar_comp()
+
 triangulo1.desenha_triangulo()
+
 circulo1.desenha_circulo()
 
 plt.axis('equal')
