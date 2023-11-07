bd71c2afbe70cc3cc1822164cefc2c1d2067f652
Natan Almeida <natan.almeida03@gmail.com>
2023-11-07 13:56:34 -0300

    oo, eda

--- EDA(2023-2)/ALGORITIMOS_DE_ORDENACAO.c
+++ EDA(2023-2)/ALGORITIMOS_DE_ORDENACAO.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 void exch(int* xp, int* yp)
 {
     int temp = *xp;
@@ -9,12 +10,12 @@ void exch(int* xp, int* yp)
 }
 
 /*
-SELECTION SORT -- O(N²) -- NÃO ESTÁVEL
+SELECTION SORT -- O(n²) -- NÃO ESTÁVEL
 1 - SELECIONAR MENOR NÚMERO
 2 - POSICIONAR: TROQUE COM O PRIMEIRO ITEM (index 0)
 3 - SELECIONAR O SEGUNDO MENOR NUMERO
 4 - POSICIONAR: TROQUE COM O SEGUNDO ITEM (index 1)
-5 - REPITA ATÉ O ULTIMO ITEM DA LISTA
+5 - REPITA PARA OS n ELEMENTOS DO VETOR
 */
 void selection_sort (int v[], int l, int r){
     int menor;
@@ -26,7 +27,7 @@ void selection_sort (int v[], int l, int r){
                 menor = j;
 
         if(i != menor)
-            exch(v[i], v[menor]);
+            exch(&v[i], &v[menor]);
     }
 }
 
@@ -39,12 +40,12 @@ void selection_sort_rec (int v[], int l, int r){
     if(primeiro != menor)
         exch(&v[i], &v[menor]);
 
-    //return selection_sort_rec (v[], ++l, r);
+    return selection_sort_rec (v, ++l, r);
     }
 }
 
 /*
-BUBBLE SORT -- O(N²) -- ESTÁVEL
+BUBBLE SORT -- O(n²) -- ESTÁVEL
 1 - DO INICIO, FLUTUAR ITEM
 2 - AO ACHAR UMA "BOLHA" MAIOR, ESTA PASSA A FLUTUAR
 3 - NO FIM, O MAIOR (OU MENOR) ESTA NO TOPO
@@ -64,4 +65,51 @@ void bubble_sort(int v[], int l, int r)
         if (swapped == false)
             break;
     }
+}
+
+/*
+INSERTION SORT -- O(n²/2) -- ESTÁVEL
+1 - INSERIR CADA ELEMENTO NA POSICAO CORRETA EM RELACAO AOS SEUS ANTECESSORES
+2 - COMPARACAO ITEM A ITEM COM SEUS ANTECESSORES
+*/
+
+void insertion_sort(int v[], int l, int r){
+    for(int i = l+1; i<=r; i++)
+    {
+        for(int j = i; j>l && v[j]<v[j-1]; j--)
+        {
+            exch(&v[j], &v[j-1]);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int t;
+    scanf("%d", &t);
+    printf("%d ELEMENTOS\n", t);
+    int *v = (int *)malloc(sizeof(int)*t);
+
+    for(int i = 0; i < t; i++)
+        scanf("%d", &v[i]);
+
+
+    if (strcmp(argv[1], "selection") == 0)
+        selection_sort(v, 0, t - 1);
+    else if (strcmp(argv[1], "selection_rec") == 0)
+        return 0;
+        //selection_sort_rec(v, 0, t-1);
+    else if (strcmp(argv[1], "bubble") == 0)
+        bubble_sort(v, 0, t-1);
+    else if (strcmp(argv[1], "insertion") == 0)
+        insertion_sort(v, 0, t-1);
+    else
+    {
+        printf("select a valid sort method\n");
+        return 0;
+    }
+
+    //for(int j = 0; j < t; j++) printf("%d ", v[j]);
+
+    return 0;
 }
